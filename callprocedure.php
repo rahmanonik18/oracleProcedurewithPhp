7154dd8233f616428eac46d3b8bc32484d55c2c2
Abdur Rahman Onik <abduronikrahman@gmail.com>
2021-05-20 20:33:45 +0600

    Update callprocedure.php

--- callprocedure.php
+++ callprocedure.php
@@ -1,10 +1,8 @@
-
 <?php
-
-$presql = 'BEGIN PC_BILL_SIGN_UPDATE(:EMPCODE,:SIGN_OBJ_CODE,:PRE_SIGN_OBJ_CODE); END;';            
-				$pstmt = oci_parse($conn,$presql);                     
-				oci_bind_by_name($pstmt,':EMPCODE',$EMP_CODE); 
-				oci_bind_by_name($pstmt,':SIGN_OBJ_CODE',$SIGN_OBJ_CODE);
-                oci_bind_by_name($pstmt,':PRE_SIGN_OBJ_CODE',$prev_sign_obj_flag);
-				oci_execute($pstmt);
+$presql = 'BEGIN PC_BILL_SIGN_UPDATE(:EMPCODE,:SIGN_OBJ_CODE,:PRE_SIGN_OBJ_CODE); END;';
+$pstmt = oci_parse($conn, $presql);
+oci_bind_by_name($pstmt, ':EMPCODE', $EMP_CODE);
+oci_bind_by_name($pstmt, ':SIGN_OBJ_CODE', $SIGN_OBJ_CODE);
+oci_bind_by_name($pstmt, ':PRE_SIGN_OBJ_CODE', $prev_sign_obj_flag);
+oci_execute($pstmt);
 ?>
